750de81ae84de68ea048dddbc25ad17e038f8e2e
mohitk09 <mohit.khotani.15csc@bml.edu.in>
2018-08-19 11:35:19 +0530

    Added retrieval of text from Image

--- Steganography.py
+++ Steganography.py
@@ -10,8 +10,8 @@ def decimal_to_binary(val):
         l.append(rem)
         val = int(val)/2
     actual_list = list(reversed(l))
-    if len(actual_list)<8:
-        for i in range(0,8-len(actual_list)):
+    if len(actual_list) < 8:
+        for i in range(0, 8-len(actual_list)):
             actual_list.insert(i, 0)
     return actual_list
 
@@ -21,7 +21,6 @@ def binary_to_decimal(val):
     for i in range(0,len(val)):
         decimal_value += val[i]*pow(2,len(val)-i-1)
     return decimal_value
-
 
 
 # here bits refers to the bit number where text will be hidden
@@ -38,7 +37,7 @@ def hide_text(img, text_file, bits):
         s = s+c
     print(s)
     list_of_characters = []
-    for i in range(0,len(s)):
+    for i in range(0, len(s)):
         val = decimal_to_binary(ord(s[i]))
         list_of_characters.append(val)
     print(list_of_characters)
@@ -47,20 +46,56 @@ def hide_text(img, text_file, bits):
         # here m and n are used to iterate the list of characters and for image iteration i and j are used
         i = 0
         j = 0
-        for m in range(0,len(list_of_characters)):
-            for n in range(0,len(list_of_characters[m]),bits):
+        num_of_pixels_used = 0 # can also be calculated by size of the text file and number of bit level of an image used
+        for m in range(0, len(list_of_characters)):
+            for n in range(0, len(list_of_characters[m]),bits):
                 val = decimal_to_binary(int(img[i][j]))
-                for v in range(0,bits): # v is for iterating the bits level in an image
+                for v in range(0, bits):  # v is for iterating the bits level in an image
                     val[len(val)-1-v] = list_of_characters[m][n+v]
+                num_of_pixels_used += 1
                 img[i][j] = binary_to_decimal(val)
-                if j<len(img[i])-1:
-                    j = j+1
+                if j < len(img[i])-1:
+                    j += 1
                 else:
                     j = 0
-                    i = i+1
+                    i += 1
+        plt.imshow(img,cmap=plt.get_cmap('gray'))
+        plt.show()
+        retrieve_text(img,bits,num_of_pixels_used,i)
+        print(num_of_pixels_used)
     else:
         print("File is too big")
 
+
+def retrieve_text(img,bits,number_of_pixels_used,rows):
+    max_pixels = 0
+    list_of_characters = []
+    l = []
+    for i in range(0,len(img)):
+        if i <= rows:
+            for j in range(0, len(img[i])):
+                val = decimal_to_binary(int(img[i][j]))
+                if max_pixels <= number_of_pixels_used:
+                    if len(l) < 8:
+                        for k in range(0, bits):
+                            l.append(val[len(val) - 1 - k])
+                            #print(val[len(val)-1-k])
+                        max_pixels += 1
+                    else:
+                        list_of_characters.append(l)
+                        l = []
+                        max_pixels += 1
+                        for k in range(0, bits):
+                            l.append(val[len(val)-1-k])
+                else:
+                    break
+
+    s = ''
+    for i in range(0,len(list_of_characters)):
+        val = binary_to_decimal(list_of_characters[i])
+        s = s+chr(val)
+    print("Retrieved text from the Image is")
+    print(s)
 
 
 img = io.imread('Pepper.tiff', as_gray=True)
@@ -70,6 +105,6 @@ for i in range(0,len(img)):
     for j in range(0,len(img[i])):
         img[i][j] = int(img[i][j])
 text_file = open('Text_to_hide.txt', 'r')
-hide_text(img, text_file, 1)
+hide_text(img, text_file, 4)
 text_file.close()
-plt.show()
+
