0f1847115034cdc20c72018f3c1204cdffd6cbc2
mohitk09 <mohit.khotani.15csc@bml.edu.in>
2018-10-08 10:39:05 +0530

    Global and Local Thresholding

--- Image_Segmentation.py
+++ Image_Segmentation.py
@@ -1,6 +1,7 @@
 from skimage import io
 import matplotlib.pyplot as plt
 import numpy as np
+'''
 img = io.imread('D:\\Study material\\7th sem\\DIP\\Test_Images\\test_image.png', as_gray=True)
 img = img*255
 
@@ -48,6 +49,68 @@ plt.imshow(new_image, cmap='gray')
 plt.subplot(2,1,2)
 plt.imshow(new_image1,cmap='gray')
 plt.show()
+'''
+def global_thresholding(test_image1):
+    test_image = np.empty([len(test_image1),len(test_image1[0])])
+    l = len(test_image)*len(test_image[0])
+    total = 0
+    for i in range(0,len(test_image)):
+        for j in range(0,len(test_image[0])):
+            total += test_image1[i][j]
+    comparison_value = total/l
+    for i in range(0,len(test_image)):
+        for j in range(0,len(test_image)):
+            if test_image1[i][j]>comparison_value:
+                test_image[i][j] = 255
+            else:
+                test_image[i][j] = 0
+    plt.imshow(test_image,cmap='gray')
+    plt.show()
+    return test_image
+def local_thresholding(test_image):
+    test_image = np.array(test_image, dtype=np.uint)
+    test_imagel = np.empty([len(test_image),len(test_image[0])])
+    for i in range(0,len(test_image)):
+        for j in range(0,len(test_image[i])):
+            if i == 0 and j==0:
+                total = (test_image[i][j]+test_image[i+1][j]+test_image[i][j+1]+test_image[i+1][j+1])
+                mean = total/4
+            elif i==0 and j== len(test_image[0])-1:
+                total = (test_image[i][j]+test_image[i+1][j]+test_image[i][j-1]+test_image[i+1][j-1])
+                mean = total/4
+            elif i == len(test_image)-1 and j == 0:
+                total = (test_image[i][j] + test_image[i-1][j] + test_image[i][j+1] + test_image[i-1][j+1])
+                mean = total / 4
+            elif i == len(test_image)-1 and j == len(test_image[0])-1:
+                total = (test_image[i][j] + test_image[i][j-1] + test_image[i-1][j] + test_image[i-1][j-1])
+                mean = total / 4
+            elif i == 0:
+                total = test_image[i][j] + test_image[i][j-1]+test_image[i][j+1]+test_image[i+1][j+1]+test_image[i+1][                          j-1]+test_image[i+1][j]
+                mean = total / 6
+            elif i == len(test_image)-1:
+                total = test_image[i][j] + test_image[i][j-1]+test_image[i][j+1]+test_image[i-1][j+1]+test_image[i-1][                          j-1]+test_image[i-1][j]
+                mean = total / 6
+            elif j == 0:
+                total = test_image[i][j] + test_image[i+1][j]+test_image[i-1][j]+test_image[i][j+1]+test_image[i-1][                          j+1]+test_image[i+1][j+1]
+                mean = total / 6
+            elif j == len(test_image[0])-1:
+                total = test_image[i][j] + test_image[i+1][j]+test_image[i-1][j]+test_image[i][j-1]+test_image[i+1][                          j-1]+test_image[i-1][j-1]
+                mean = total / 6
+            else:
+                total = test_image[i][j] + test_image[i+1][j] + test_image[i-1][j] + test_image[i][j-1]+test_image[i][j+1] +                     test_image[i+1][j+1]+test_image[i+1][j-1]+test_image[i-1][j-1]+test_image[i-1][j+1]
+                mean = total / 9
+            if test_image[i][j]>mean:
+                test_imagel[i][j] = 255
+    plt.imshow(test_imagel,cmap='gray')
+    plt.show()
+    return test_imagel
 
-
-
+img = io.imread('D:\\Study material\\7th sem\\DIP\\BinDataset\\BinDataset\\Texture.tiff', as_gray=True)
+img *= 255
+img1 = global_thresholding(img)
+img2 = local_thresholding(img)
+plt.subplot(1,2,1)
+plt.imshow(img1,cmap='gray')
+plt.subplot(1,2,2)
+plt.imshow(img2,cmap='gray')
+plt.show()
